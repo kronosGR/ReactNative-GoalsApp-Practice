9343c41eb8e91281d02153f033d323f0a1f99715
kronosGR <geo.elgeo@gmail.com>
2022-04-09 06:12:08 +0200

    started

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View>
+        <TextInput placeholder='Your course goal' />
+        <Button title='Add Goal' />
+      </View>
+      <View>
+        <Text>List of goals</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
