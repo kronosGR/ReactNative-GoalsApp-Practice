6cb2d4744d01814b2ab404189585df7c0602c41e
kronosGR <geo.elgeo@gmail.com>
2022-04-11 07:10:55 +0200

    add  GoalItem component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, StyleSheet, TextInput, View } from 'react-native';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -26,11 +27,7 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem />;
           }}
           alwaysBounceVertical='true'
           keyExtractor={(item, idex) => {
@@ -66,14 +63,5 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-    padding: 8,
-  },
-  goalText: {
-    color: 'white',
   },
 });
---
+++ components/GoalItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const GoalItem = () => {
+  return (
+    <View style={styles.goalItem}>
+      <Text style={styles.goalText}>{itemData.item.text}</Text>
+    </View>
+  );
+};
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    padding: 8,
+  },
+  goalText: {
+    color: 'white',
+  },
+});
